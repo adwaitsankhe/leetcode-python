bd04f263c2c65369a9b65ae1a8e1fa85845c7ee3
Adwait Y Sankhé <adwaitsankhe@Adwaits-MacBook-Pro.local>
2019-12-14 21:10:07 -0500

    valid parenthesis

---
+++ ValidParenthesis.py
@@ -0,0 +1,18 @@
+class ValidParenthesis:
+    def isValid(self, s) -> bool:
+        bracket_map = {"[": "]", "{": "}", "(": ")"}
+        stack = []
+        for bracket in s:
+            if bracket in bracket_map:
+                stack.append(bracket_map[bracket])
+            elif not stack or stack.pop() != bracket:
+                return False
+
+        return not stack
+
+if __name__ == "__main__":
+    assert(ValidParenthesis().isValid("()") == True)
+    assert(ValidParenthesis().isValid("([)") == False)
+    assert (ValidParenthesis().isValid("[(])") == False)
+    assert (ValidParenthesis().isValid("()[]{}") == True)
+
